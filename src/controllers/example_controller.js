c69744ca823e0046d1c7d3e0da3a30fa65a9ed0c
Jesher Minelli <jesherdevsk8@gmail.com>
2024-01-01 20:50:24 -0300

    first commit to add clipboard example

--- src/controllers/example_controller.js
+++ src/controllers/example_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "It works!"
+    this.element.textContent = "Exploring StimulusJS"
   }
 }
