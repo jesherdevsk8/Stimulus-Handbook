b50157392f3a411acf2a43875a7c0b6038a963a2
Jesher Minelli <jesherdevsk8@gmail.com>
2024-01-01 21:28:23 -0300

    improve feature: generate pin when load page

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -3,7 +3,32 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "source" ]
 
+  connect() {
+    this.generateAndSetPIN();
+  }
+
+  generateAndSetPIN() {
+    const generatedPIN = this.generatePIN(4);
+    this.sourceTarget.value = generatedPIN;
+  }
+
+  generatePIN(length) {
+    const characters = '0123456789';
+    let pin = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      pin += characters.charAt(randomIndex);
+    }
+
+    return pin;
+  }
+
   copy() {
-    navigator.clipboard.writeText(this.sourceTarget.value)
+    navigator.clipboard.writeText(this.sourceTarget.value).then(() => {
+      alert("Texto copiado para a área de transferência!");
+    }).catch((error) => {
+      console.error('Erro ao copiar para a área de transferência:', error);
+    });
   }
 }
